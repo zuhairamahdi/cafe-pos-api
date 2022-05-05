d81e979bbe4de073e14eedd2a7213366b441665c
Zuhair Abdulla <zuhairamahdi@gmail.com>
2022-05-05 16:13:21 +0300

    Migrate db to pstgresql instead of sqlite

--- config/database.js
+++ config/database.js
@@ -1,11 +1,20 @@
 const path = require('path');
 
 module.exports = ({ env }) => ({
-  connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "postgres",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "cafe"),
+        username: env("DATABASE_USERNAME", "postgres"),
+        password: env("DATABASE_PASSWORD", "password"),
+        schema: env("DATABASE_SCHEMA", "public"),
+      },
+      options: {},
     },
-    useNullAsDefault: true,
   },
 });
