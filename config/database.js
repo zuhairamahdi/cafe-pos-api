d371873cfd697913b4d446a1ecbfed1e050d73e6
Zuhair Abdulla <zuhairamahdi@gmail.com>
2022-05-05 16:26:07 +0300

    Install  dialect and Fix db connection issue

--- config/database.js
+++ config/database.js
@@ -1,20 +1,19 @@
-const path = require('path');
+
 
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "cafe"),
-        username: env("DATABASE_USERNAME", "postgres"),
-        password: env("DATABASE_PASSWORD", "password"),
-        schema: env("DATABASE_SCHEMA", "public"),
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: "localhost",
+      port: 5432,
+      database: "cafe",
+      user: "postgres",
+      password: "password",
+      ssl: {
+        rejectUnauthorized: false
       },
-      options: {},
     },
+    debug: false,
   },
 });
+
